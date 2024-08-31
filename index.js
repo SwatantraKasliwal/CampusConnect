9fc0d1b8572033e0276b5905e52267da954ce00c
SwatantraKasliwal <swatantrakasliwal25@gmail.com>
2024-08-31 23:01:39 +0530

    Created the admin login form , event and subevent form

--- index.js
+++ index.js
@@ -31,12 +31,24 @@ app.post("/adminlogin",async (req, res) => {
   const userName = req.body.username;
   const password = req.body.password;
   const username = await db.query("SELECT * FROM adminlogin WHERE id=$1 AND password=$2",[userName,password]);
-  if (username.rows.length > 0){
-    res.send("you succesfully login");
+  const data = username.rows;
+  if (data.length > 0){
+    res.render("adminform.ejs",{
+      communityName: userName,
+      adminId:data[0].id
+    });
   }else{
     res.send("try again");
   }
+});
 
+app.post("/subeventform", async(req, res) =>{
+ const subEventName = req.body.subeventname;
+ const subEventDetails = req.body.subeventdetails;
+ const subEventDate = req.body.subeventdate;
+ const adminId = req.body.adminId;
+ await db.query('INSERT INTO subevent (subevent_name,subevent_details,subevent_date,user_id,event_id) VALUES ($1, $2, $3, $4, 1)',[subEventName, subEventDetails, subEventDate,adminId ]);
+ res.send("Sub event added successfully");
 });
 
 app.listen(port, () => {
--- queries.sql
+++ queries.sql
@@ -1,7 +1,18 @@
-CREATE TABLE adminlogin(
-    id VARCHAR(20) PRIMARY KEY,
-    password VARCHAR(20) NOT NULL 
+-- Admin table
+create table adminlogin(
+	id serial primary key,
+	admin_id varchar(20) unique not null,
+	password varchar(50) not null
 );
 
-INSERT INTO adminlogin (id, password) VALUES ('user001', '12345678');
 
+INSERT INTO adminlogin (admin_id, password) VALUES ('user001', '12345678');
+
+-- Event Table
+CREATE TABLE event(
+    event_id SERIAL PRIMARY KEY,
+	event_name VARCHAR(250) NOT NULL,
+	event_details VARCHAR(600),
+	event_date DATE,
+	user_id INT REFERENCES adminlogin(id)
+);
